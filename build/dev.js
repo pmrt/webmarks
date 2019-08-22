e09b910ed66083a0f707b435ad2d2f2a678c21ff
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-08-22 10:57:41 +0200

    webpack: fixed dev paths

--- build/dev.js
+++ build/dev.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const rootPath = path.join(__dirname, '../');
+const distPath = path.join(rootPath, '/dist');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const name = 'webtabs';
 
 module.exports = {
-    context: rootPath,
-    entry: `./src/${name}.js`,
+    context: path.join(rootPath, '/test'),
+    entry: path.join(rootPath, `/src/${name}.js`),
     devtool: 'eval',
     output: {
         filename: `${name}.js`,
-        path: path.join(rootPath, '/test')
+        path: distPath,
     },
     mode: 'development',
     devServer: {
         host: '127.0.0.1',
-        contentBase: path.join(rootPath, '/test'),
+        contentBase: path.join(rootPath, '/test/static'),
         watchContentBase: true,
         compress: true,
         port: 8001,
@@ -40,8 +41,8 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./test/static/index.html",
+            template: "./static/index.html",
             filename: "index.html"
-        }),
+        })
     ],
 }

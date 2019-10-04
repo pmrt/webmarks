78fc19c259e6e0af76d1be94f9701c343b61184f
Pedro J. Martínez <pedrojmrt@gmail.com>
2019-10-04 12:00:59 +0200

    web-dev-sv: allow local connections

--- build/dev.js
+++ build/dev.js
@@ -23,7 +23,7 @@ module.exports = {
     },
     mode: 'development',
     devServer: {
-        host: '127.0.0.1',
+        host: '0.0.0.0',
         contentBase: rootPath,
         watchContentBase: true,
         compress: true,
